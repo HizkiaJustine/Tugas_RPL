c3d661ddb29bc5798611392a76771cb808cd9678
HizkiaJustine <zeusxdhd@gmail.com>
2024-11-27 11:14:02 +0700

    bikin migration table account

--- database/migrations/0001_01_01_000000_create_account_table.php
+++ database/migrations/0001_01_01_000000_create_account_table.php
@@ -11,25 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+        Schema::create('account', function (Blueprint $table) {
+            $table->string(column: 'AccountID')->primary();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum(column: 'Role', allowed: ['Administrator', 'Dokter', 'Pasien', 'Kasir']);
             $table->rememberToken();
-            $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->string('AccountID')->nullable()->index();
+            $table->foreign('AccountID')->references('AccountID')->on('account');
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
@@ -43,7 +37,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
