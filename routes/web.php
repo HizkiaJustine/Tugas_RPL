4cdbfd8004f5f4ef5a62131ef194e8194c4b2a61
HizkiaJustine <zeusxdhd@gmail.com>
2024-12-01 22:36:30 +0700

    benerin route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PasienController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +20,8 @@ Route::delete('/delete_pasien/{id}', [PasienController::class, 'destroy'])->name
 Route::put('/update_pasien/{id}', [PasienController::class, 'update'])->name('update_pasien');
 Route::get('/pasien/create', [PasienController::class, 'create'])->name('create_pasien');
 Route::post('/pasien', [PasienController::class, 'store'])->name('store_pasien');
+
+Route::get('/rekammedis/{id}', [RekamMedisController::class, 'show'])->name('rekammedis');
 
 // Route::get('/payment', function () {
 //     return view('payment', ['title' => 'Home Page / Payment Management', 'name' => 'Payment Management', 'records' => Payment::all()]);
