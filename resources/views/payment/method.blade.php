8db6e034c44d4fb8a77818c5504f71e799fac2e8
dtann26 <davidtannurwael@gmail.com>
2024-12-02 23:21:22 +0700

    fixing tampilan

--- resources/views/payment/method.blade.php
+++ resources/views/payment/method.blade.php
@@ -1,23 +1,23 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Select Payment Method</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <h1>Select Payment Method</h1>
-    <form action="{{ route('payment_success') }}" method="get">
-        <label>
-            <input type="radio" name="payment_method" value="credit_card"> Credit Card
-        </label><br>
-        <label>
-            <input type="radio" name="payment_method" value="paypal"> PayPal
-        </label><br>
-        <label>
-            <input type="radio" name="payment_method" value="bank_transfer"> Bank Transfer
-        </label><br>
-        <a href="/payment/success"></a>
-        <button type="submit">Proceed to Payment</button>
-    </form>
+    <div class="container">
+        <h1>Select Payment Method</h1>
+        <form action="{{ route('payment_success') }}" method="get">
+            <div class="form-group">
+                <label for="payment_method">Payment Method</label>
+                <select name="payment_method" id="payment_method" class="form-control">
+                    <option value="gopay">Gopay</option>
+                    <option value="paypal">PayPal</option>
+                    <option value="qris">QRIS</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Proceed to Payment</button>
+        </form>
+    </div>
 </body>
 </html>
