9666810a6c78e754de03d0fc6835ec74060345e6
dtann26 <davidtannurwael@gmail.com>
2024-12-02 14:55:23 +0700

    testing

--- resources/views/test_middleware.blade.php
+++ resources/views/test_middleware.blade.php
@@ -6,7 +6,5 @@
 <body>
     <h1>Middleware Test Page</h1>
     <p>If you can see this page, the middleware is working correctly for your role.</p>
-    @component('components.auth-pasien')
-    @endcomponent
 </body>
 </html>
